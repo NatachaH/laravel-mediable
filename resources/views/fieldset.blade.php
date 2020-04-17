1cb199a90fe52601d45259230b3ba2ccaeded162
Natacha Herth <info@natachaherth.ch>
2020-04-17 19:05:12 +0200

    corr

--- resources/views/fieldset.blade.php
+++ resources/views/fieldset.blade.php
@@ -1,1 +1,15 @@
-<x-bs-dynamic :legend="$legend" folder="mediable::media" :current="$currentMedia" :is-dynamic="$is-dynamic" :min="$min" :max="$max" delete-name="media_to_delete[]" />
+<x-bs-dynamic :legend="$legend" :current="$currentMedia" :is-dynamic="$isDynamic" :min="$min" :max="$max" delete-name="media_to_delete[]">
+
+  <x-slot name="form">
+
+    <input type="hidden" name="media_to_add[KEY][type]" value="{{ $type }}" />
+
+    @if($hasName)
+      <x-bs-input class="w-50 mr-2" label="Name" name="media_to_add[KEY][name]" :placeholder="__('mediable::mediable.input.name')" />
+    @endif
+
+    <x-bs-input-file :class="$hasName ? 'w-50 mr-2' : 'w-100 mr-2'" label="File" name="media_to_add[KEY][file]" :placeholder="__('mediable::mediable.input.placeholder')" :button="__('mediable::mediable.input.button')" />
+
+  </x-slot>
+
+</x-bs-dynamic>
--- resources/views/media/dynamic/form.blade.php
+++
@@ -1,7 +0,0 @@
-<input type="hidden" name="media_to_add[KEY][type]" value="{{ $type }}" />
-
-@if($hasName)
-  <x-bs-input class="w-50 mr-2" label="Name" name="media_to_add[KEY][name]" :placeholder="__('mediable::mediable.input.name')" />
-@endif
-
-<x-bs-input-file :class="$hasName ? 'w-50 mr-2' : 'w-100 mr-2'" label="File" name="media_to_add[KEY][file]" :placeholder="__('mediable::mediable.input.placeholder')" :button="__('mediable::mediable.input.button')" />
