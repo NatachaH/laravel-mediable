6b8c26116a2b91f36af6a8fbabd39d2f56bfd2ba
Natacha Herth <info@natachaherth.ch>
2020-04-24 17:34:43 +0200

    corr download button in input group

--- resources/views/includes/dynamic-media.blade.php
+++ resources/views/includes/dynamic-media.blade.php
@@ -5,8 +5,8 @@
 <x-bs-input :class="$options['hasName'] ? 'w-50 mr-2' : 'w-100 mr-2'" :label="__('mediable::media.input.file.label')" :name="'media_to_update['.$item->id.'][file]'" :value="$item->filename" readonly :input-group="$options['hasDownload']">
   @if($options['hasDownload'])
     <x-slot name="after">
-      <a href="{{ $item->url }}" class="btn {{ config('mediable.buttons.download.class') }}" download target="_blank" aria-label="{{ __(config('mediable.buttons.download.label')) }}">
-        {!! config('mediable.buttons.download.value') ?? __(config('mediable.buttons.download.label')) !!}
+      <a href="{{ $item->url }}" class="btn {{ config('mediable.buttons.download-input-group.class') }}" download target="_blank" aria-label="{{ __(config('mediable.buttons.download-input-group.label')) }}">
+        {!! config('mediable.buttons.download-input-group.value') ?? __(config('mediable.buttons.download-input-group.label')) !!}
       </a>
     </x-slot>
   @endif
